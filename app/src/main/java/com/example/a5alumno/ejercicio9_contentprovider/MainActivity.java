26a53b1c155e3655adb0c3da353cfa71275ce3bf
ferran <fmundovidal@gmail.com>
2016-11-28 10:29:03 +0100

    initial button implementations done

--- app/src/main/java/com/example/a5alumno/ejercicio9_contentprovider/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/ejercicio9_contentprovider/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.a5alumno.ejercicio9_contentprovider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnQueryDb = (Button) this.findViewById(R.id.btn_query_db);
+        btnQueryDb.setOnClickListener(this);
+        Button btnAddDb = (Button)this.findViewById(R.id.btn_add_db);
+        btnAddDb.setOnClickListener(this);
+        Button btnDeleteDb = (Button)this.findViewById(R.id.btn_delete_db);
+        btnDeleteDb.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_query_db:
+                break;
+            case R.id.btn_add_db:
+                break;
+            case R.id.btn_delete_db:
+                break;
+        }
     }
 }
